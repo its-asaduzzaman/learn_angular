f9d717184673f24e2be6de97e894f187fd747625
its-asaduzzaman <samrat.asad@gmail.com>
2024-05-13 12:35:01 +0600

    first init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-app';
+  serverId = 10;
+  serverStatus = 'offline';
+  username ='';
+  serverName = '';
+  serverCreationStatus = "No server was created";
+  serverCreated = false;
+  constructor(){
+    this.serverStatus =Math.random()> .50 ? 'online':'offline';
+  }
+  getServerStatus(){
+    return this.serverStatus;
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created! Name is '+ this.serverName;
+    this.serverCreated = true;
+  }
+  getColor(){
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
